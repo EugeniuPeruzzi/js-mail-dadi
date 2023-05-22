447584f3e7bf5a0481742e7dc8d7309e276cc4a4
EugeniuPeruzzi <jaka1997@live.it>
2023-05-22 15:58:46 +0200

    email pattern

--- js-email/js/script.js
+++ js-email/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let btn_input = document.getElementById('submit');
+
+btn_input.addEventListener('click', function(){
+    
+    let user_email = document.getElementById('user_email').value;
+    
+    console.log(user_email);
+
+})
+
+
