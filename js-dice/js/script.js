bb6064c70cf112559fc2c781867d91f295ce115b
EugeniuPeruzzi <jaka1997@live.it>
2023-05-22 19:26:03 +0200

    fixed

--- js-dice/js/script.js
+++ js-dice/js/script.js
@@ -1,6 +1,24 @@
 'use strict';
 
-let player = Math.floor(Math.random() * 6 + 1);
-console.log(player)
-let computer = Math.floor(Math.random() * 6 + 1);
-console.log(computer)
+let play = document.getElementById('btn').addEventListener('click',  function(){
+    
+    let player = Math.floor(Math.random() * 6 + 1);
+    
+    let computer = Math.floor(Math.random() * 6 + 1);
+    
+    if (player < computer){
+        document.getElementById('result').innerHTML = `<p>Il Tuo risultato:${player}</p>
+                                                       <p>Opponente: ${computer}</p> 
+                                                       <p>Sconfitta, ritenta..magari ti va meglio.</p>`
+    }
+    else if (player == computer){
+        document.getElementById('result').innerHTML =  `<p>Il Tuo risultato:${player}</p>
+                                                        <p>Opponente: ${computer}</p> 
+                                                        <p>Hai pareggiato, 1 possibilita su 1000.</p>`
+    }
+    else {
+        document.getElementById('result').innerHTML =  `<p>Il Tuo risultato:${player}</p>
+                                                        <p>Opponente: ${computer}</p> 
+                                                        <p>Vittoria!</p>`
+    }
+})
