447ee94fd96e6631b31e2decea60892c58c0694d
EugeniuPeruzzi <jaka1997@live.it>
2023-05-22 16:43:21 +0200

    adjusted button type added array

--- js-email/js/script.js
+++ js-email/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
-
+// dicchiarto le variabili e array con le email registrate precedentemente. 
 let btn_input = document.getElementById('submit');
+let email = ['esempio@boolean.it', 'fabrizio@boolean.it', 'enrico@boolean.it', 'davide@boolean.it', 'jacopo@boolean.it', 'eugeniu@boolean.it']
+console.log(email)
 
 btn_input.addEventListener('click', function(){
     
